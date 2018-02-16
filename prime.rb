b9a7c39f24217b12b23d5f7103f0ea744c44c32c
Grayson Cowing <grayson.cowing@gmail.com>
2018-02-16 15:56:39 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -4,7 +4,7 @@ def prime?(num)
   if num > 0
     array =* (2...(num-1))
   elsif num<0
-    array =* (-2...((-num)+1))
+    array =* (2...((-num)-1))
   array.each {|x| num % x == 0 ? divisor = x : nil}
   if divisor != 0
     return false
