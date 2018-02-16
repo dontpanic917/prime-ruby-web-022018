6b3084a4cff0577f4b701cd889bce8f6454ffac9
Grayson Cowing <grayson.cowing@gmail.com>
2018-02-16 16:02:15 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,9 @@
 # Add  code here!
 def prime?(num)
   divisor = 0
-  if num > 0
+  if num == 1
+    return false
+  elsif num > 0
     array =* (2...(num-1))
     array.each {|x| num % x == 0 ? divisor = x : nil}
     if divisor != 0
@@ -9,8 +11,6 @@ def prime?(num)
     elsif divisor == 0
       return true
     end
-  elsif num == 1
-    return false
   elsif num<0
     return false
   else
