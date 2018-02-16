bb622c10ef72ecb7b557803e3eb608759816e992
Grayson Cowing <grayson.cowing@gmail.com>
2018-02-16 15:51:46 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,6 +2,6 @@
 def prime?(num)
   isprime=false
   array =* (2...(num-1))
-  array.each {|x| num % x == 0 ? isprime = false : isprime = true}
-  return !isprime
+  array.each {|x| num % x == 0 ? divisor = x : puts 'no divisor'}
+  return isprime
 end
